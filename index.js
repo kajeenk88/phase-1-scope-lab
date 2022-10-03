fd2444ba556b45177c85ecbe68ff3af6da226ecc
kajeen khalid <khalidkajeen@gmail.com>
2022-10-03 18:11:41 -0500

    commit tree 4a7e60ab47ae0c0e8091a980a831ca3a4b6b3933

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = "sour fred";
+
+const currentUser = "Grace Hopper";
+function upperCaseCustomerName() {
+  return (customerName = customerName.toUpperCase());
+}
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "sour fred";
+}
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+function setBestCustomer() {
+  var bestCustomer = "not bob";
+  window.bestCustomer = bestCustomer;
+}
+
+function overwriteBestCustomer(newFavorite) {
+  bestCustomer = newFavorite;
+}
